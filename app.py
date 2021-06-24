fa641cd3417c9e32663267c85c5cad7ce6d35216
Arnab Chattopadhyay <59999832+akbc1221@users.noreply.github.com>
2021-06-24 20:53:00 +0530

    err5

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, flash, request, url_for, redirect, session
 import numpy as np
-import pandas as pd
+# import pandas as pd
 import re
 import os
 import tensorflow as tf
